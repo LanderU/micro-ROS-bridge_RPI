bc6037c53c90983b07165be13ea5f663be886649
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-05-19 10:05:42 +0200

    Update cc_script.sh

--- micro-ROS-Agent_Cross-Compilation/cc_script.sh
+++ micro-ROS-Agent_Cross-Compilation/cc_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
 apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 apt-get update
