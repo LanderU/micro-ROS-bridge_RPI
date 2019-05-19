d5975430e0c58389cdf3f0a77ac3fedc79c4c8fa
Lander Usategui San Juan <lander@erlerobotics.com>
2019-05-19 09:58:04 +0200

    Update cc_script.sh

--- micro-ROS-Agent_Cross-Compilation/cc_script.sh
+++ micro-ROS-Agent_Cross-Compilation/cc_script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-echo micro-ROS agent cross-compailing for Raspberry-Pi 3
-
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-sudo apt-get update
-sudo apt install wget git python3-vcstool
+echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
+apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+apt-get update
+apt install wget git python3-vcstool
 
 mkdir -p ~/micro-ros_rpi/agent_ws/src #Agent ws
 cd ~/micro-ros_rpi
 
-echo Downloading tools for cross-compilation
 git clone https://github.com/micro-ROS/polly
 git clone https://github.com/micro-ROS/ros2_raspbian_tools
 
@@ -23,7 +19,7 @@ cat Dockerfile.bootstrap | docker build -t ros2-raspbian:crosscompiler -
 
 mkdir -p ~/micro-ros_rpi/rpi-root
 cd ~/micro-ros_rpi/ros2_raspbian_tools
-sudo tar -C ~/micro-ros_rpi/rpi-root -xvf ros2-raspbian-rootfs.tar
+tar -C ~/micro-ros_rpi/rpi-root -xvf ros2-raspbian-rootfs.tar
 
 
 cd ..
